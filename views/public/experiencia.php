07a806847e114ab45001ca3f99f79dab1fdc2250
Diego Ramírez <diegosay777@gmail.com>
2022-08-29 16:52:13 -0600

    arreglos css

--- views/public/experiencia.php
+++ views/public/experiencia.php
@@ -16,33 +16,7 @@ webSite::header('Experiencia');
     <div class="row d-flex justify-content-center align-items-center h-100">
         <p class="tituloExperiencia">EXPERIENCIA</p>
 
-        <div class="row gx-5">
-            <div class="col col-md-4 col-lg-4 col-xl-4 col-sm-12 col-12 ">
-                <div class="p-3 layout2">
-
-
-
-
-
-
-                </div>
-            </div>
-            <div class="col col-md-4 col-lg-4 col-xl-4 col-sm-12 col-12">
-                <div class="p-3 layout2">
-
-
-
-                </div>
-            </div>
-
-            <div class="col col-md-4 col-lg-4 col-xl-4 col-sm-12 col-12">
-                <div class="p-3 layout2">
-
-
-
-                </div>
-            </div>
-        </div>
+        
     </div>
 
 </div>
