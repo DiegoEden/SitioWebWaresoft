1bddb3f0238520bd311377d2b52bea72d61a49a4
Diego Ramírez <diegosay777@gmail.com>
2022-08-29 17:12:26 -0600

    Inicio casos de exito

--- views/public/experiencia.php
+++ views/public/experiencia.php
@@ -13,10 +13,30 @@ webSite::header('Experiencia');
 <br>
 
 <div class="container" id="formContainer">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-        <p class="tituloExperiencia">EXPERIENCIA</p>
+    <p class="tituloExperiencia">EXPERIENCIA</p>
 
-        
+    <div class="row justify-content-center animate__animated animate__backInUp" id="proyectos">
+        <div class="animate__animated animate__bounceIn col-xl-4 col-md-4 col-sm-12 col-xs-12 mt-4 d-flex justify-content-center align-items-center text-center">
+            <div class="tarjeta">
+                <!-- Fila para Imagen -->
+                <div class="row">
+                    <div class="col-12">
+                    </div>
+                </div>
+                <!-- Fila para Información -->
+                <div class="row mt-2">
+                    <div class="col-12 ">
+
+                    </div>
+                </div>
+
+
+                <!-- Fin de Tarjeta -->
+            </div>
+        </div>
+
+
+
     </div>
 
 </div>
